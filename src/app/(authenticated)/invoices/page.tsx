6274f2e94d6141fe54d5a7aaf8794230d2847c5b
Frans Siswanto <franssiswanto@gmail.com>
2025-05-24 00:31:44 +0800

    feat: update invoice page layout
    Restructures invoice page using a column layout.
    Adds descriptive text, positions the create button,
    and wraps the invoice table with a partner provider.

--- src/app/(authenticated)/invoices/page.tsx
+++ src/app/(authenticated)/invoices/page.tsx
@@ -1,23 +1,33 @@
 import React from "react";
-import {PageContent} from "@/core/presentations/components/page-content";
-import {PageHeading} from "@/core/presentations/components/page-heading";
-import {CreateNewInvoiceButton} from "@/app/(authenticated)/invoices/_components/create-new-button";
-import {InvoiceTableImpl} from "@/app/(authenticated)/invoices/_components/invoice-table-impl";
-import {InvoiceProvider} from "@/features/invoice/presentations/providers/invoice";
+import { PageContent } from "@/core/presentations/components/page-content";
+import { PageHeading } from "@/core/presentations/components/page-heading";
+import { InvoiceProvider } from "@/features/invoice/presentations/providers/invoice";
+import { ListPartnerProvider } from "@/features/partner/presentation/providers/list-partner";
+import { InvoiceTableImpl } from "@/app/(authenticated)/invoices/_components/invoice-table-impl";
+import { CreateNewInvoiceButton } from "@/app/(authenticated)/invoices/_components/create-new-button";
 
 export default function InvoiceMainPage() {
   return (
     <InvoiceProvider>
-      <div className="flex justify-between items-center">
-        <div className="flex-1">
-          <PageHeading>Kumpulan Faktur</PageHeading>
-        </div>
-        <div className="mx-auto px-4 sm:flex-nowrap sm:px-6 lg:px-8">
-          <CreateNewInvoiceButton/>
-        </div>
-      </div>
+      <PageHeading>Kumpulan Faktur</PageHeading>
       <PageContent>
-        <InvoiceTableImpl/>
+        <div className="flex-col space-y-4">
+          <div className="flex flex-row justify-between">
+            <div className="flex-1">
+              <p className="text-gray-500">
+                Lihat semua invoice Anda dalam satu tempat. Kelola, lacak, dan kirim invoice dengan mudah dan efisien.
+              </p>
+            </div>
+            <div className="flex-1 flex justify-end">
+              <CreateNewInvoiceButton />
+            </div>
+          </div>
+          <div className="flex-1">
+            <ListPartnerProvider>
+              <InvoiceTableImpl />
+            </ListPartnerProvider>
+          </div>
+        </div>
       </PageContent>
     </InvoiceProvider>
   );
