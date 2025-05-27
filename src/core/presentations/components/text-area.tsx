cd07c27b053605f4b46340137c7b74bff0aef72b
Frans Siswanto <franssiswanto@gmail.com>
2025-05-27 10:21:17 +0800

    feat: add new dialog and text area components
    Introduces a new dialog for adding items and a reusable text area component with label integration.
    
    Enhances UI flexibility by encapsulating text input behavior and styling.

---
+++ src/core/presentations/components/text-area.tsx
@@ -0,0 +1,36 @@
+"use client";
+
+import React from "react";
+import { Label } from "@/core/presentations/components/label";
+
+export type TextAreaProps =
+  {
+    title: string;
+    description?: string;
+    boldLabel?: boolean;
+    value?: string;
+    onChange?: (value: string) => void;
+    rows?: number
+  }
+  & Omit<React.DetailedHTMLProps<React.TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement>, "value" | "onChange" | "rows">;
+
+export function TextArea(props: TextAreaProps) {
+  return (
+    <div>
+      <Label
+        title={props.title}
+        description={props.description}
+        bold={props.boldLabel}
+      />
+      <div className="mt-2">
+        <textarea
+          {...props}
+          rows={props.rows ?? 4}
+          className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-primary-default sm:text-sm/6"
+          value={props.value}
+          onChange={e => props.onChange?.(e.target.value)}
+        />
+      </div>
+    </div>
+  );
+}
