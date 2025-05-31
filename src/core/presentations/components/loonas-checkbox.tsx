9343e26659350fe4c8eab6006bae307b9ea26ef5
Frans Siswanto <franssiswanto@gmail.com>
2025-05-31 16:13:36 +0800

    feat: add custom checkbox component
    Introduces a new checkbox component with customizable state handling
    and clickable label support to improve UI consistency.

---
+++ src/core/presentations/components/loonas-checkbox.tsx
@@ -0,0 +1,42 @@
+import { Checkbox, Field } from "@headlessui/react";
+import React from "react";
+
+interface LoonasCheckboxProps {
+  checked: boolean;
+  onChange?: (checked: boolean) => void | Promise<void>;
+  children?: React.ReactNode;
+}
+
+export function LoonasCheckbox(props: LoonasCheckboxProps) {
+  const handleLabelClick = () => {
+    if (!props.onChange) return;
+    props.onChange(!props.checked);
+  };
+
+  return (
+    <Field className="flex flex-1 flex-row space-x-2">
+      <Checkbox
+        checked={props.checked}
+        onChange={props.onChange}
+        className="group data-[checked]:bg-primary-default data-[checked]:border-primary-default mt-1 flex size-5 flex-row items-center justify-center rounded-sm border-1 border-gray-500 bg-white"
+      >
+        <svg
+          fill="none"
+          viewBox="0 0 14 14"
+          className="pointer-events-none size-3.5 stroke-white p-0.5 group-has-disabled:stroke-gray-950/25"
+        >
+          <path
+            d="M3 8L6 11L11 3.5"
+            strokeWidth={3}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="opacity-0 group-data-[checked]:opacity-100"
+          />
+        </svg>
+      </Checkbox>
+      <div className="flex-1 select-none" onClick={handleLabelClick}>
+        {props.children}
+      </div>
+    </Field>
+  );
+}
