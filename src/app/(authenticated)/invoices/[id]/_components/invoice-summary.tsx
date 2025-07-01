4402480b343e9b165a5e4fc089eb7152108a3d6a
Frans Siswanto <franssiswanto@gmail.com>
2025-07-01 07:57:16 +0000

    feat: replace DetailItem with CardDetailItem in InvoiceSummary for consistent layout

--- src/app/(authenticated)/invoices/[id]/_components/invoice-summary.tsx
+++ src/app/(authenticated)/invoices/[id]/_components/invoice-summary.tsx
@@ -1,6 +1,6 @@
-import { DetailItem } from "@/app/(authenticated)/invoices/[id]/@outgoingDetail/_components/detail-item";
 import { InvoiceStatusChip } from "@/app/(authenticated)/invoices/_components/invoice-status-chip";
 import { Card } from "@/core/presentations/components/card";
+import { CardDetailItem } from "@/core/presentations/components/card-detail-item";
 import { IDRFormatter } from "@/core/utilities/currency/domain/formatters/idr-formatter";
 import { InvoiceStatus } from "@/features/invoice/domain/entities/invoice";
 import { InvoiceType } from "@/features/invoice/domain/enums/invoice-type";
@@ -18,18 +18,18 @@ export function InvoiceSummary(props: InvoiceSummaryProps) {
   return (
     <Card>
       <div className="flex flex-col space-y-4">
-        <DetailItem label="ID Faktur">{props.id}</DetailItem>
+        <CardDetailItem label="ID Faktur">{props.id}</CardDetailItem>
         <div className="flex flex-row justify-between space-x-4 text-left">
-          <DetailItem label="Status Faktur">
+          <CardDetailItem label="Status Faktur">
             <InvoiceStatusChip status={props.status} />
-          </DetailItem>
-          <DetailItem label="Jenis Faktur">
+          </CardDetailItem>
+          <CardDetailItem label="Jenis Faktur">
             {props.type === InvoiceType.OUTGOING ? "Faktur Keluaran" : "Faktur Masukan"}
-          </DetailItem>
-          <DetailItem label="Nilai Faktur">{IDRFormatter.toCurrency(props.total)}</DetailItem>
-          <DetailItem label="Tanggal Dibuat">
+          </CardDetailItem>
+          <CardDetailItem label="Nilai Faktur">{IDRFormatter.toCurrency(props.total)}</CardDetailItem>
+          <CardDetailItem label="Tanggal Dibuat">
             {props.createdAt.setLocale("id-ID").toFormat("dd MMMM yyyy HH:mm")}
-          </DetailItem>
+          </CardDetailItem>
         </div>
       </div>
     </Card>
--- src/core/presentations/components/card-detail-item.tsx
+++ src/core/presentations/components/card-detail-item.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-interface DetailItemProps {
+interface CardDetailItemProps {
   label: string;
   children: React.ReactNode;
 }
 
-export function DetailItem(props: DetailItemProps) {
+export function CardDetailItem(props: CardDetailItemProps) {
   return (
     <div className="flex flex-col">
       <div className="flex-1 text-xs font-semibold text-gray-900">{props.label}</div>
