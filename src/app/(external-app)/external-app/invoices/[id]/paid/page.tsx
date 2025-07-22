a210ce9e770b629d4196b21115ed8f37bc5f22b3
Frans Siswanto <franssiswanto@gmail.com>
2025-07-22 12:07:44 +0800

    Adds paid invoice confirmation page
    Introduces a dedicated page to confirm successful invoice payments, enhancing user experience.
    
    Moves the `done-animation.json` file to a more generic location in the core presentations, allowing for re-use across different parts of the application.

---
+++ src/app/(external-app)/external-app/invoices/[id]/paid/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import DoneAnimation from "@/core/presentations/static-files/done-animation.json";
+import { LogoImage } from "@/core/presentations/components/logo-image";
+import { InvoiceMetadataImpl } from "@/app/(external-app)/external-app/invoices/[id]/pay/_components/invoice-metadata-impl";
+import { PaymentSummaryImpl } from "@/app/(external-app)/external-app/invoices/[id]/pay-in-detail/_components/payment-summary-impl";
+import { Card } from "@/core/presentations/components/card";
+import { Player } from "@lottiefiles/react-lottie-player";
+import React from "react";
+
+export default function PaidPage() {
+  return (
+    <div className="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+      <div className="flex flex-col space-y-4">
+        <div className="flex-1 self-start">
+          <LogoImage />
+        </div>
+        <div className="flex-1">
+          <InvoiceMetadataImpl />
+        </div>
+        <div className="flex flex-1 flex-col space-y-4">
+          <div className="flex flex-row space-x-4">
+            <div className="flex-2">
+              <Card>
+                <div className="flex flex-col items-center space-y-4">
+                  <h1 className="text-base font-semibold text-gray-900">Pembayaran Berhasil</h1>
+                  <div className="h-35 w-35">
+                    <Player autoplay loop src={DoneAnimation} />
+                  </div>
+                  <div className="w-3/4 text-center">
+                    <p>Dana Anda sedang kami teruskan ke pengirim invoice. Terima kasih telah menggunakan Loonas.</p>
+                  </div>
+                </div>
+              </Card>
+            </div>
+            <div className="flex-1">
+              <PaymentSummaryImpl />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
