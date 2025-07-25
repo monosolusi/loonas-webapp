32d4b6a53c42ce08dae0778ae1b7c3340494605b
Frans Siswanto <franssiswanto@gmail.com>
2025-07-25 09:39:41 +0800

    Implements account verification check
    Adds a component that checks if the selected account is verified
    before allowing access to invoice features.
    
    The component redirects users to the account verification page
    if their account is not yet verified or if an error occurs
    while retrieving the verification status.
    
    Also refactors account not verified page to use clsx.

--- src/app/(authenticated)/invoices/_components/must-verified-account.tsx
+++ src/app/(authenticated)/invoices/_components/must-verified-account.tsx
@@ -1,49 +1,23 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useSelectedAccountProvider } from "@/features/authentication/presentation/providers/selected-account";
 import { ErrorCodes, ServerError } from "@/core/resources/server-error";
-import { LocalStorageSessionService } from "@/features/authentication/data/sources/local-storage-session";
-import { SessionRepositoryImpl } from "@/features/authentication/data/repositories/session";
-import { AccountServiceImpl } from "@/features/account/data/sources/account";
-import { AccountRepositoryImpl } from "@/features/account/data/repositories/account";
-import {
-  RetrieveAccountVerificationWorkUseCase,
-  RetrieveAccountVerificationWorkUseCaseParams,
-} from "@/features/account/domain/usecases/retrieve-account-verification-work";
-import { DataFailed } from "@/core/resources/data-state";
-import { VerificationStatus } from "@/features/account/domain/enums/verification-status";
 import { useRouter } from "next/navigation";
+import { VerificationStatus } from "@/features/account/domain/enums/verification-status";
+import { useGetAccountVerificationWork } from "@/features/account/presentation/hooks/use-get-account-verification-work";
 
 export function MustVerifiedAccount(props: { children: React.ReactNode }) {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [error, setError] = useState<Error>();
-  const { selectedAccount } = useSelectedAccountProvider();
   const router = useRouter();
-
-  const checkIfSelectedAccountIsVerified = async () => {
-    if (!selectedAccount) throw new ServerError(ErrorCodes.NOT_FOUND);
-    const sessionService = new LocalStorageSessionService();
-    const sessionRepository = new SessionRepositoryImpl(sessionService);
-    const accountService = new AccountServiceImpl();
-    const accountRepository = new AccountRepositoryImpl(accountService);
-    const retrieve = new RetrieveAccountVerificationWorkUseCase(accountRepository, sessionRepository);
-    const retrieveParams = new RetrieveAccountVerificationWorkUseCaseParams(selectedAccount.id);
-    const verificationWork = await retrieve.execute(retrieveParams);
-    if (verificationWork instanceof DataFailed) throw verificationWork.error;
-    if (!verificationWork.data) throw new ServerError(ErrorCodes.INVALID_INSTANCE);
-    if (verificationWork.data.latestStatus !== VerificationStatus.COMPLETED) {
-      throw new ServerError(ErrorCodes.ACCOUNT_NOT_VERIFIED);
-    }
-  };
-
+  const { selectedAccount } = useSelectedAccountProvider();
+  const { verificationWork, loading, error } = useGetAccountVerificationWork({ accountId: selectedAccount?.id });
+  
   useEffect(() => {
-    if (!selectedAccount) return;
-    setLoading(true);
-    checkIfSelectedAccountIsVerified()
-      .then(() => setLoading(false))
-      .catch((err) => setError(err));
-  }, [selectedAccount]);
+    if (!verificationWork) return;
+    if (verificationWork.latestStatus !== VerificationStatus.COMPLETED) {
+      router.replace(`/invoices/account-not-verified`);
+    }
+  }, [verificationWork]);
 
   useEffect(() => {
     if (!error) return;
@@ -54,6 +28,7 @@ export function MustVerifiedAccount(props: { children: React.ReactNode }) {
     } else throw error;
   }, [error]);
 
-  if (loading) return null;
+  if (!verificationWork || loading) return null;
+  if (verificationWork.latestStatus !== VerificationStatus.COMPLETED) return null;
   return <>{props.children}</>;
 }
--- src/app/(authenticated)/invoices/account-not-verified/page.tsx
+++ src/app/(authenticated)/invoices/account-not-verified/page.tsx
@@ -6,38 +6,34 @@ import { CheckBadgeIcon, UserIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import { useSelectedAccountProvider } from "@/features/authentication/presentation/providers/selected-account";
 import { PageContent } from "@/core/presentations/components/page-content";
-
-function classNames(...classes: any) {
-  return classes.filter(Boolean).join(" ");
-}
+import clsx from "clsx";
 
 export default function AccountNotVerifiedPage() {
   const { selectedAccount } = useSelectedAccountProvider();
 
   return (
     <PageContent>
-      <div className="w-full h-full flex items-center justify-center">
+      <div className="flex h-full w-full items-center justify-center">
         <div className="mx-auto max-w-lg">
           <h2 className="text-base font-semibold text-gray-900">Akun Kamu Sedang Diverifikasi</h2>
           <p className="mt-1 text-sm text-gray-500">
             Akun kamu saat ini sedang dalam proses verifikasi agar nantinya bisa digunakan untuk membuat faktur digital
             secara aman. Kamu bisa pantau status verifikasi dengan memilih tombol Cek Status di bawah ini. Setelah
-            proses
-            verifikasi selesai, kami akan langsung informasikan lewat email, ya.
+            proses verifikasi selesai, kami akan langsung informasikan lewat email, ya.
           </p>
           <ul role="list" className="mt-6 divide-y divide-gray-200 border-t border-b border-gray-200">
             <li className="group">
               <div className="group relative flex items-start space-x-3 py-4">
-                <div className="shrink-0 mt-1">
-              <span
-                className={classNames(
-                  "group-data-[disabled=true]:grayscale",
-                  "bg-primary-default",
-                  "inline-flex size-10 items-center justify-center rounded-lg"
-                )}
-              >
-                <CheckBadgeIcon aria-hidden="true" className="size-6 text-white" />
-              </span>
+                <div className="mt-1 shrink-0">
+                  <span
+                    className={clsx(
+                      "group-data-[disabled=true]:grayscale",
+                      "bg-primary-default",
+                      "inline-flex size-10 items-center justify-center rounded-lg",
+                    )}
+                  >
+                    <CheckBadgeIcon aria-hidden="true" className="size-6 text-white" />
+                  </span>
                 </div>
                 <div className="min-w-0 flex-1">
                   <div className="text-sm font-medium text-gray-900 group-data-[disabled=true]:text-gray-400">
@@ -47,8 +43,8 @@ export default function AccountNotVerifiedPage() {
                     </Link>
                   </div>
                   <p className="text-sm text-gray-500 group-data-[disabled=true]:text-gray-400">
-                    Pengen tahu status terbaru akunmu? Klik tombol ini buat ngecek status verifikasi akun kamu
-                    sekarang juga. Kami akan selalu update status verifikasimu secara real-time di halaman ini.
+                    Pengen tahu status terbaru akunmu? Klik tombol ini buat ngecek status verifikasi akun kamu sekarang
+                    juga. Kami akan selalu update status verifikasimu secara real-time di halaman ini.
                   </p>
                 </div>
                 <div className="shrink-0 self-center">
@@ -61,10 +57,10 @@ export default function AccountNotVerifiedPage() {
             </li>
             <li className="group">
               <div className="group relative flex items-start space-x-3 py-4">
-                <div className="shrink-0 mt-1">
-              <span className="bg-green-600 inline-flex size-10 items-center justify-center rounded-lg">
-                <UserIcon aria-hidden="true" className="size-6 text-white" />
-              </span>
+                <div className="mt-1 shrink-0">
+                  <span className="inline-flex size-10 items-center justify-center rounded-lg bg-green-600">
+                    <UserIcon aria-hidden="true" className="size-6 text-white" />
+                  </span>
                 </div>
                 <div className="min-w-0 flex-1">
                   <div className="text-sm font-medium text-gray-900 group-data-[disabled=true]:text-gray-400">
