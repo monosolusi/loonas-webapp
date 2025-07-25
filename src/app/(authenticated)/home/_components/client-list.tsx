5a111db211e4374f87cfec09195eb6b1694b730e
Frans Siswanto <franssiswanto@gmail.com>
2025-07-25 12:33:26 +0800

    Displays "no client" state when no clients exist
    Improves the user experience by displaying a helpful message and suggestion when no clients are available. This guides the user to add a new client. Also handles error state from the `useListPartner` hook.

--- src/app/(authenticated)/home/_components/client-list.tsx
+++ src/app/(authenticated)/home/_components/client-list.tsx
@@ -28,7 +28,6 @@ export function ClientList(props: ClientListProps) {
       {props.data.map((client) => (
         <li key={client.id} className="overflow-hidden rounded-xl border border-gray-200">
           <div className="flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 p-6">
-
             <div className="text-sm/6 font-medium text-gray-900">{client.name}</div>
             <Menu as="div" className="relative ml-auto">
               <MenuButton className="-m-2.5 block p-2.5 text-gray-400 hover:text-gray-500">
@@ -37,7 +36,7 @@ export function ClientList(props: ClientListProps) {
               </MenuButton>
               <MenuItems
                 transition
-                className="absolute right-0 z-10 mt-0.5 w-32 origin-top-right rounded-md bg-white py-2 ring-1 shadow-lg ring-gray-900/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
+                className="absolute right-0 z-10 mt-0.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 transition focus:outline-hidden data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in"
               >
                 <MenuItem>
                   <a
