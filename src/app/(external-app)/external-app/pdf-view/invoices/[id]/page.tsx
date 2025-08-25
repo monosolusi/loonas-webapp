6668bde79bdb93312d6723e7c94d19fec39d856e
Frans Siswanto <franssiswanto@gmail.com>
2025-08-25 20:16:21 +0800

    Feat: Enables public invoice view
    Adds a new page for publicly viewing invoices.
    The invoice preview component is made more flexible by allowing additional class names.
    This allows external applications to display invoices.

---
+++ src/app/(external-app)/external-app/pdf-view/invoices/[id]/page.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { useParams } from "next/navigation";
+import { useGetPublicOutgoingInvoice } from "@/features/invoice/presentations/hooks/use-get-public-outgoing-invoice";
+import { InvoicePreview } from "@/app/(authenticated)/invoices/_components/invoice-preview";
+
+export default function InvoicePage() {
+  const { id } = useParams<{ id: string }>();
+  const { loading, invoice } = useGetPublicOutgoingInvoice({ id });
+
+  if (loading || !invoice) return null;
+  return (
+    <InvoicePreview
+      invoice={{
+        id: invoice.id,
+        invoiceNumber: invoice.invoiceNumber,
+        invoiceDate: invoice.invoiceDate,
+        dueDate: invoice.dueDate,
+        note: invoice.note,
+        tnc: invoice.tnc,
+      }}
+      items={invoice.items.map((item) => ({
+        name: item.name,
+        description: item.description,
+        qty: item.qty,
+        price: item.price,
+        taxType: item.taxType,
+        tax: item.tax,
+        taxBase: item.taxBase,
+        total: item.total,
+        discountType: item.discountType,
+        discount: item.discount,
+      }))}
+      recipient={{
+        name: invoice.recipient.name,
+        email: invoice.recipient.email,
+        phoneNumber: invoice.recipient.phoneNumber,
+      }}
+      sender={{
+        name: invoice.sender.name,
+        address: invoice.sender.address,
+      }}
+      signature={{
+        signerName: invoice.signature.signerName,
+        url: invoice.signature.url,
+      }}
+    />
+  );
+}
