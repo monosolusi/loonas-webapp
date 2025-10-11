512114d455af0a6d643c6d8df642e4e3ea5a09ff
Frans Siswanto <franssiswanto@gmail.com>
2025-10-11 21:31:34 +0800

    feat: add QRIS pay-in detail page with QR code display
    Introduces a new QRIS payment detail page for invoices, featuring a styled box that displays a QR code using the newly added qrcode.react dependency. Enhances user experience by providing clear payment instructions and branding.

---
+++ src/app/(authenticated)/invoices/[id]/qris-pay-in-detail/_components/qris-pay-in-detail-box.tsx
@@ -0,0 +1,40 @@
+import { Card } from "@/core/presentations/components/card";
+import { QRCodeSVG } from "qrcode.react";
+
+export function QrisPayInDetailBox() {
+  return (
+    <Card className="relative overflow-hidden">
+      {/* aksen merah */}
+      <div aria-hidden className="pointer-events-none absolute inset-0 -z-0">
+        {/* kiri-atas */}
+        <div className="absolute top-[25%] left-[-25%] h-[220px] w-[220px] rotate-45 bg-[#e7413a]"></div>
+        {/* kanan-bawah */}
+        <div className="absolute right-[-25%] bottom-[-25%] h-[250px] w-[250px] rotate-45 bg-[#e7413a]"></div>
+      </div>
+
+      <div className="relative z-10 flex flex-col gap-y-8">
+        <div className="flex flex-col gap-y-8">
+          <div className="flex flex-1 flex-row items-center justify-between">
+            <img
+              src="https://res.cloudinary.com/monosolusi/image/upload/v1760171194/loonas/web-assets/quick-response-code-indonesia-standard-qris-seeklogo_lxox7l.png"
+              className="h-10"
+            />
+            <img
+              src="https://res.cloudinary.com/monosolusi/image/upload/v1760188238/loonas/web-assets/Gerbang_Pembayaran_Nasional_logo_baa0h3.svg"
+              className="h-12"
+            />
+          </div>
+          <div className="text-center text-xl font-semibold">PT. Tumbuh Adidaya Perkasa</div>
+          <div className="flex flex-col items-center gap-y-4">
+            <QRCodeSVG value="some-random-text" size={256} />
+            <div className="flex flex-col gap-y-1 text-center">
+              <div className="uppercase">SATU QRIS Untuk Semua</div>
+              <div className="text-sm">Cek aplikasi penyelenggara</div>
+              <div className="text-sm">Di: www.aspi-qris.id</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Card>
+  );
+}
---
+++ src/app/(authenticated)/invoices/[id]/qris-pay-in-detail/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { PageContent } from "@/core/presentations/components/page-content";
+import { TextHeadingWithUnderline } from "@/core/presentations/components/text-heading-with-underline";
+import { RemainingPaymentTime } from "@/core/presentations/components/remaining-payment-time";
+import { DateTime } from "luxon";
+import { PaymentDetail } from "@/app/(authenticated)/invoices/_components/payment-detail";
+import { QrisPayInDetailBox } from "@/app/(authenticated)/invoices/[id]/qris-pay-in-detail/_components/qris-pay-in-detail-box";
+
+export default function QrisPayInDetailPage() {
+  return (
+    <PageContent>
+      <div className="flex flex-col gap-y-5">
+        <TextHeadingWithUnderline>Harap Lakukan Pembayaran</TextHeadingWithUnderline>
+        <div className="flex flex-row gap-x-4">
+          <div className="flex flex-1 flex-col gap-y-4">
+            {/*  RemainingPaymentTime dan QrisPayInDetailBox */}
+            <RemainingPaymentTime deadline={DateTime.now().plus({ days: 1 })} />
+            <QrisPayInDetailBox />
+          </div>
+          <div className="flex flex-1">
+            {/*  PaymentDetail */}
+            <PaymentDetail
+              invoiceId="1234567890"
+              receiverName="Test"
+              bankName="BCA"
+              accountNumber="1234567890"
+              accountHolderName="AAa"
+              total={100000}
+              fee={1000}
+              totalPayment={101000}
+              showActions={false}
+            />
+          </div>
+        </div>
+      </div>
+    </PageContent>
+  );
+}
