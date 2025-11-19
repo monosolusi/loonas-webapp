ac5b32fc10d26c7c1d52e4f9d80d22fd7dd1f8c3
Frans <franssiswanto@gmail.com>
2025-11-19 22:45:18 +0700

    Feat: Displays invalid credentials alert
    Adds an alert to inform users of incorrect login attempts.
    
    The alert is displayed when the provided email or password
    does not match any existing user credentials. It enhances the
    user experience by providing immediate feedback on login
    errors.

--- src/app/(authentication)/sign-in/_components/credential-form.tsx
+++ src/app/(authentication)/sign-in/_components/credential-form.tsx
@@ -42,38 +42,3 @@ export function CredentialForm() {
     </form>
   );
 }
-
-// "use client";
-//
-// import React from "react";
-// import { EmailInput } from "@/core/presentations/components/email-input";
-// import { PasswordInput } from "@/core/presentations/components/password-input";
-// import { useSignInProvider } from "@/features/authentication/presentation/providers/sign-in";
-// import { FilledButton } from "@/core/presentations/components/filled-button";
-// import Link from "next/link";
-//
-// export function CredentialForm() {
-//   const { email, password, setEmail, setPassword, login, loading } = useSignInProvider();
-//
-//   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
-//     e.preventDefault();
-//     if (!loading) login?.();
-//   }
-//
-//   return (
-//     <form onSubmit={handleSubmit} className="space-y-6">
-//       <EmailInput value={email} onChange={setEmail} required />
-//       <PasswordInput value={password} onChange={setPassword} />
-//
-//       <div className="flex items-center justify-between">
-//         <div className="text-sm/6">
-//           <Link href="/reset-password" className="font-semibold text-primary-600 hover:text-primary-500">
-//             Lupa Kata Sandi?
-//           </Link>
-//         </div>
-//       </div>
-//
-//       <FilledButton loading={loading}>Masuk</FilledButton>
-//     </form>
-//   );
-// }
--- src/app/(authentication)/sign-in/_components/invalid-cred-alert.tsx
+++ src/app/(authentication)/sign-in/_components/invalid-cred-alert.tsx
@@ -1,24 +1,16 @@
 "use client";
 
-import { XCircleIcon } from "@heroicons/react/20/solid";
 import { useSignInProvider } from "@/features/authentication/presentation/providers/sign-in";
+import Image from "next/image";
 
 export function InvalidCredAlert() {
   const { showInvalidCred } = useSignInProvider();
 
   if (!showInvalidCred) return <></>;
   return (
-    <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-[480px]">
-      <div className="rounded-md bg-red-50 p-4">
-        <div className="flex">
-          <div className="shrink-0">
-            <XCircleIcon aria-hidden="true" className="size-5 text-red-400" />
-          </div>
-          <div className="ml-3">
-            <p className="text-sm text-red-800">Oops! Email atau password-nya nggak cocok nih. Coba lagi ya!</p>
-          </div>
-        </div>
-      </div>
+    <div className="bg-error-50 border-error-100 mt-8 flex flex-row gap-3 rounded-lg border p-4">
+      <Image src="/assets/images/exclamation-circle-w20-h20.svg" alt="Exclamation Icon" width={20} height={20} />
+      <span className="text-error-500 text-base">Email atau kata sandi salah. Silakan coba lagi.</span>
     </div>
   );
 }
