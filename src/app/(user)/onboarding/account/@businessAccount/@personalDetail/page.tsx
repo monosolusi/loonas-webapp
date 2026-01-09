eb155115d681f3601272f19c5693fe2928abca35
Frans Siswanto <franssiswanto@gmail.com>
2026-01-09 22:16:18 +0800

    feat: add company profile form to business onboarding
    Introduces a company profile form for business onboarding, allowing users to input company name, email, and phone number. Enhances onboarding experience by collecting essential business information.

--- src/app/(user)/onboarding/account/@businessAccount/@personalDetail/page.tsx
+++ src/app/(user)/onboarding/account/@businessAccount/@personalDetail/page.tsx
@@ -1,3 +1,24 @@
+import { TextInput } from "@/core/presentations/components/text-input";
+import Image from "next/image";
+import React from "react";
+
 export default function BusinessDetailPage() {
-  return <></>;
+  return (
+    <>
+      <div className="mb-6 flex flex-col">
+        <span className="text-lg leading-6 font-medium text-neutral-500">Profil Perusahaan</span>
+        <span className="text-sm leading-5 font-medium text-neutral-200">Lengkapi identitas dasar perusahaan.</span>
+      </div>
+      <div className="mb-8 flex flex-col gap-4">
+        <TextInput label="Nama Perusahaan" type="text" placeholder="Masukan nama perusahaan Anda." />
+        <TextInput
+          label="Email Perusahaan"
+          type="email"
+          placeholder="Masukan email perusahaan Anda."
+          leftIcon={<Image src="/assets/images/email-icon-w20-h20.svg" alt="Email Icon" width={20} height={20} />}
+        />
+        <TextInput label="Nomor Telpon" type="tel" placeholder="Masukan nomor telpon Anda." />
+      </div>
+    </>
+  );
 }
