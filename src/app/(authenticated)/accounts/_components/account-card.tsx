9b45dfd37c3b15f18808a85b7800a05beb27f1e1
Frans Siswanto <franssiswanto@gmail.com>
2026-01-13 18:46:25 +0800

    feat: add "Buat Akun Baru" card to accounts page
    Introduces an interactive card for adding new accounts, enhancing discoverability and user experience. Updates styles and adds a plus icon asset for visual consistency.

--- src/app/(authenticated)/accounts/_components/account-card.tsx
+++ src/app/(authenticated)/accounts/_components/account-card.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export function AccountCard() {
   return (
-    <div className="flex flex-col gap-y-6 rounded-lg border border-neutral-200 bg-white p-6">
+    <div className="flex w-[256px] flex-col gap-y-6 rounded-lg border border-neutral-200 bg-white p-6">
       {/* Icons */}
-      <div className="flex w-[250px] flex-row items-start justify-between">
+      <div className="flex flex-row items-start justify-between">
         <div className="bg-success-50 border-success-100 text-success-400 flex size-12 flex-col items-center justify-center rounded-lg shadow-sm">
           <Image
             src="/assets/images/suitcase-icon-success-400-w24-h24.svg"
@@ -29,7 +29,7 @@ export function AccountCard() {
 
       {/*  Action */}
       <button type="button">
-        <div className="hover:bg-primary-300/10 hover:text-primary-400 flex cursor-pointer flex-row justify-between rounded-lg border border-neutral-200 bg-white px-3 py-2 transition duration-100 ease-in-out focus:outline-none data-closed:opacity-0">
+        <div className="hover:bg-primary-300/10 hover:text-primary-400 flex cursor-pointer flex-row justify-between rounded-lg border border-neutral-200 bg-white px-3 py-2 transition transition-all duration-100 ease-in-out ease-out focus:outline-none data-closed:opacity-0">
           <div className="text-sm leading-5 font-medium">Masuk Dashboard</div>
           <Image
             src="/assets/images/arrow-tailed-right-icon-neutral-500-w16-h16.svg"
