91f75268fd392b6dba2f0ef41f3639563928620a
Frans Siswanto <franssiswanto@gmail.com>
2026-01-13 19:12:53 +0800

    feat: refactor account card UI and add dynamic status badge
    Replaces hardcoded account type icons and labels with a reusable component that adapts based on account data. Introduces a dynamic account status badge displaying real-time verification status and outcome. Enhances UI flexibility and improves maintainability for future account types and states.

--- src/app/(authenticated)/accounts/_components/account-card.tsx
+++ src/app/(authenticated)/accounts/_components/account-card.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import { AccountTypeEntity } from "@/features/account/domain/types/account-type";
+import { AccountStatusBadge } from "@/app/(authenticated)/accounts/_components/account-status-badge";
+import { AccountCardIcon } from "@/app/(authenticated)/accounts/_components/account-card-icon";
 
 type AccountCardProps = {
   account: AccountTypeEntity;
@@ -9,27 +11,12 @@ export function AccountCard(props: AccountCardProps) {
   return (
     <div className="flex w-[256px] flex-col gap-y-6 rounded-lg border border-neutral-200 bg-white p-6">
       {/* Icons */}
-      <div className="flex flex-row items-start justify-between">
-        <div className="bg-success-50 border-success-100 text-success-400 flex size-12 flex-col items-center justify-center rounded-lg shadow-sm">
-          <Image
-            src="/assets/images/suitcase-icon-success-400-w24-h24.svg"
-            alt="Suitcase Icon"
-            width={24}
-            height={24}
-          />
-        </div>
-        <div className="bg-success-50 border-success-100 rounded-md border px-2 py-1">
-          <div className="text-success-400 text-xs leading-4">Bisnis</div>
-        </div>
-      </div>
+      <AccountCardIcon account={props.account} />
 
       {/*  Account Information */}
       <div className="flex flex-col gap-y-1">
-        <div className="truncate text-xl leading-7 font-bold">PT Maju Jaya</div>
-        <div className="flex flex-row items-center gap-x-2">
-          <div className="bg-success-300 size-2 rounded-full"></div>
-          <div className="text-sm leading-5 text-neutral-300">Aktif</div>
-        </div>
+        <div className="truncate text-xl leading-7 font-bold">{props.account.fullName}</div>
+        <AccountStatusBadge account={props.account} />
       </div>
 
       {/*  Action */}
